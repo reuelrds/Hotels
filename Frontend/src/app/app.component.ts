69bcd55787f68b612ec8c058d57dd148c18b22f6
Reuel Dsilva <reuel.dsilva@yahoo.com>
2021-08-22 19:05:25 -0500

    Changed Website Name

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'athena-root',
+  selector: 'hotels-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Hotel Booking App';
