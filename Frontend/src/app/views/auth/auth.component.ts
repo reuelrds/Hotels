d55be355b34c0de2bcc9fc1d10a89f2fcbc0ddc2
Reuel Dsilva <reuel.dsilva@yahoo.com>
2021-10-05 11:01:34 -0500

    Completed Auth UI

--- Frontend/src/app/views/auth/auth.component.ts
+++ Frontend/src/app/views/auth/auth.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  isLoginFormVisible = true;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleOverlay() {
+    console.log(this.isLoginFormVisible);
+    this.isLoginFormVisible = !this.isLoginFormVisible;
+
+    console.log(this.isLoginFormVisible);
+  }
 }
